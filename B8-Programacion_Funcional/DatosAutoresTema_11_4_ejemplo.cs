1f3f4ddf6c6fb4aa83b289d80a1a04365f32d687
juanjobalmis <juanjobalmis@users.noreply.github.com>
2022-05-17 20:58:58 +0000

    deploy: programacioniesbalmis/programacioniesbalmis@0e41366f229d62eda2b19c08fe98bb422c042d5a

---
+++ B8-Programacion_Funcional/DatosAutoresTema_11_4_ejemplo.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatosAutoresTema_11_4
+{
+    public class Autor
+    {
+        public string Nombre { get; init; }
+        public string Nacionalidad { get; init; }
+        public DateTime Muerte { get; init; }
+        public IEnumerable<Libro> Libros { get; init; }
+
+        public override string ToString() =>
+        $"Nombre: {Nombre}\nNacionalidad: {Nacionalidad}\nMuerte: {Muerte.ToShortDateString()}\n" +
+        $"Libros:\n\t{string.Join("\n\t", Libros)}";
+    }
+    public class Libro
+    {
+        public string Titulo { get; init; }
+        public int Año { get; init; }
+        public int Paginas { get; init; }
+
+        public override string ToString() =>
+        $"Titulo: {Titulo,-37}  Año: {Año,-4}  Páginas: {Paginas}";
+    }
+
+    public static class Datos
+    {
+        public static IEnumerable<Autor> Autores
+        {
+            get
+            {
+                yield return new()
+                {
+                    Nombre = "William Shakespeare",
+                    Nacionalidad = "Inglesa",
+                    Muerte = new DateTime(1616, 5, 3),
+                    Libros = new Libro[]
+                    {
+                        new Libro()
+                        {
+                            Titulo = "Macbeth",
+                            Año = 1623,
+                            Paginas = 128
+                        },
+                        new Libro()
+                        {
+                            Titulo = "La tempestad",
+                            Año = 1611,
+                            Paginas = 160
+                        }
+                    }
+                };
+                yield return new()
+                {
+                    Nombre = "Miguel de Cervantes",
+                    Nacionalidad = "Española",
+                    Muerte = new DateTime(1616, 6, 22),
+                    Libros = new Libro[]
+                    {
+                        new Libro()
+                        {
+                            Titulo = "Don Quijote de la Mancha",
+                            Año = 1605,
+                            Paginas = 1376
+                        },
+                        new Libro()
+                        {
+                            Titulo = "La Galatea",
+                            Año = 1585,
+                            Paginas = 664
+                        },
+                        new Libro()
+                        {
+                            Titulo = "Los trabajos de Persiles y Sigismunda",
+                            Año = 1617,
+                            Paginas = 888
+                        },
+                        new Libro()
+                        {
+                            Titulo = "Novelas ejemplares",
+                            Año = 1613,
+                            Paginas = 1160
+                        }
+                    }
+                };
+                yield return new()
+                {
+                    Nombre = "Fernando de Rojas",
+                    Nacionalidad = "Española",
+                    Muerte = new DateTime(1541, 2, 7),
+                    Libros = new Libro[]
+                    {
+                        new Libro()
+                        {
+                            Titulo = "La Celestina",
+                            Año = 1500,
+                            Paginas = 160
+                        }
+                    }
+                };
+            }
+        }
+    }
+
+    class Principal
+    {
+
+        static void Main()
+        {
+            string SeparadorDato = "\n" + new string('-', 80) + "\n";
+
+            Console.WriteLine(
+                    SeparadorDato 
+                    + string.Join(SeparadorDato, Datos.Autores) 
+                    + SeparadorDato);
+        }
+    }
+}
