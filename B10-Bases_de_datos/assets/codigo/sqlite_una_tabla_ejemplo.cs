447fb0bde269ca0c625fc7b51cc32f1a95ead95a
juanjobalmis <juanjobalmis@users.noreply.github.com>
2025-03-27 11:07:35 +0000

    deploy: programacioniesbalmis/programacioniesbalmis@9e29e1977ed1a6b2a48a662ee403217b8f51eb89

---
+++ B10-Bases_de_datos/assets/codigo/sqlite_una_tabla_ejemplo.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using Microsoft.Data.Sqlite;
+
+public class Biblioteca
+{
+    public static void CrearTablaLibros(SqliteConnection conexion)
+    {
+        string borrarTabla = "DROP TABLE IF EXISTS libro";
+        using (SqliteCommand comando = new(borrarTabla, conexion))
+        {
+            comando.ExecuteNonQuery();
+        }
+        string crearTabla = """
+            CREATE TABLE libro (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                titulo VARCHAR(60),
+                autor VARCHAR(60)
+            );
+            """;
+        using (SqliteCommand comando = new(crearTabla, conexion))
+        {
+            comando.ExecuteNonQuery();
+        }
+    }
+
+    public static void AñadeLibros(SqliteConnection conexion)
+    {
+        string insertarDatos = """
+            INSERT INTO libro (titulo, autor) VALUES
+            ('Macbeth', 'William Shakespeare'),
+            ('La Celestina (Tragicomedia de Calisto y Melibea)', 'Fernando de Rojas'),
+            ('El Lazarillo de Tormes', 'Anónimo'),
+            ('20.000 Leguas de Viaje Submarino', 'Julio Verne'),
+            ('Alicia en el País de las Maravillas', 'Lewis Carrol'),
+            ('Cien Años de Soledad', 'Gabriel García Márquez'),
+            ('La tempestad', 'William Shakespeare');
+            """;
+
+        using SqliteCommand comando = new(insertarDatos, conexion);
+        comando.ExecuteNonQuery();
+    }
+
+
+    static void VerLibros(SqliteConnection conexion)
+    {
+        string verLibros = "SELECT * FROM libro";
+        using SqliteCommand query = new(verLibros, conexion);
+        using SqliteDataReader rs = query.ExecuteReader();
+
+        string separador = new(
+            $"| {new('-', 3),-3} | {new('-', 55),-55} | {new('-', 25),-25} |\n"
+        );
+
+        StringBuilder salida = new StringBuilder(separador)
+        .Append($"| {"Id",-3} | {"Título",-55} | {"Autor",-25} |\n")
+        .Append(separador);
+        while (rs.Read())
+        {
+            salida.Append(
+                $"| {rs["id"],-3} | {rs["titulo"],-55} | {rs["autor"],-25} |\n"
+            );
+        }
+        salida.Append(separador);
+
+        Console.WriteLine(salida);
+    }
+
+    public static void Main()
+    {
+        try
+        {
+            string cadenaConexion = "Data Source=biblioteca_1t.db";
+            using SqliteConnection conexion = new(cadenaConexion);
+            conexion.Open();
+            CrearTablaLibros(conexion);
+            AñadeLibros(conexion);
+            VerLibros(conexion);
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
