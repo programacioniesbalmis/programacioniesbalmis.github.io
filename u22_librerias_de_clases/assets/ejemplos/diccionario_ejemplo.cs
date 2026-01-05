2872994472470c2b6c31f1c77c7dadd8507f9eba
juanjobalmis <juanjobalmis@users.noreply.github.com>
2026-01-05 18:05:45 +0000

    deploy: programacioniesbalmis/programaciondam@1a7eaae941e4496647041ecde54ab554b88ff0a2

---
+++ u22_librerias_de_clases/assets/ejemplos/diccionario_ejemplo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using WeCantSpell.Hunspell;
+
+public class Program 
+{
+    static void Main() 
+    {
+        Console.Write("Cargando diccionario...\n");
+        string rutaEjecucion = Regex.Match(
+                                        input: Directory.GetCurrentDirectory(),
+                                        pattern: @"^(?<ruta>.*?)(?=\\bin)",
+                                        options: RegexOptions.IgnoreCase
+                                    ).Groups["ruta"].Value;       
+        WordList diccionario = WordList.CreateFromFiles(Path.Combine(rutaEjecucion, "Spanish.dic"));
+        string palabra;
+        do
+        {
+            Console.Write("Introduce una palabra: ");
+            palabra = Console.ReadLine()!;         
+
+            string mensaje;
+            // Si la palabra no está en el diccionario en español.
+            if (!diccionario.Check(palabra))
+            {
+                mensaje = $"{palabra} no es correcta.\n";
+                // Le pido al objeto diccionario en castellano que me devuelva
+                // un array de sugerencias posibles a partir de la palabra introducida.
+                string[] sugerencias = [.. diccionario.Suggest(palabra)];
+                if (sugerencias.Length > 0)
+                    mensaje += $"¿Quisiste decir {string.Join(", ", sugerencias)}?\n";
+            }
+            else
+                mensaje = $"{palabra} es correcta.\n";
+            Console.WriteLine(mensaje);
+
+        } while (palabra != "adios");
+    }
+}
